4ce62dea2adcf041c30600804319c405ea7d84d6
DatNguyen602 <nxuandat@epicsoft4.com>
2025-05-21 00:01:33 +0700

    Ffff

--- models/User.js
+++ models/User.js
@@ -1,15 +1,19 @@
 // models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
 const UserSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
+  email:    { type: String, unique: true, required: true },  // thêm email
   password: { type: String, required: true },
-  role:    { type: mongoose.Schema.Types.ObjectId, ref: 'Role' },
-  agency:  { type: mongoose.Schema.Types.ObjectId, ref: 'Agency' }
+  role:     { type: mongoose.Schema.Types.ObjectId, ref: 'Role' },
+  agency:   { type: mongoose.Schema.Types.ObjectId, ref: 'Agency' }
 });
-UserSchema.pre('save', async function(){
+
+UserSchema.pre('save', async function() {
   if(this.isModified('password')){
     this.password = await bcrypt.hash(this.password, 10);
   }
 });
+
 module.exports = mongoose.model('User', UserSchema);
